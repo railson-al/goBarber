f2287b9cce39d2c66875ae54a1d314c2169e5e75
Railson Almeida <railsonalmeida.dev@gmail.com>
2020-09-22 09:01:22 -0300

    Inicio do back-end, cadastro de usuário + criptografia

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 class App {
   constructor() {
     this.server = express();
@@ -13,10 +15,9 @@ class App {
     this.server.use(express.json());
   }
 
-  routes() {  
+  routes() {
     this.server.use(routes);
   }
-
 }
 
 export default new App().server;
